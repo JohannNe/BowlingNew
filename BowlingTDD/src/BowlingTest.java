bca4f873763263adfa1e7e025d07421bcd22e478
JohannN <johannneufeld@gmx.net>
2018-03-01 15:33:04 +0100

    Test cases added for functions "giveAdditionalPointsForSpare" and "giveAdditionalPointsForStrike".

--- BowlingTDD/src/BowlingTest.java
+++ BowlingTDD/src/BowlingTest.java
@@ -22,6 +22,20 @@ public class BowlingTest {
 	}
 	
 	@Test
+	public void giveAdditionalPointsForStrikeFollowedByZeros() throws Exception {
+		ArrayList<Integer> strikeSequence = new ArrayList<Integer>();
+		strikeSequence.addAll(Arrays.asList(10,10,10,10,0,0,10,10,10,10,10,10));
+		assertEquals(0, BowlingGame.giveAdditionalPointsForStrike(strikeSequence, 3));
+	}
+	
+	@Test
+	public void giveAdditionalPointsForStrikeFollowedByThreeAndFive() throws Exception {
+		ArrayList<Integer> strikeSequence = new ArrayList<Integer>();
+		strikeSequence.addAll(Arrays.asList(10,10,10,10,3,5,10,10,10,10,10,10));
+		assertEquals(8, BowlingGame.giveAdditionalPointsForStrike(strikeSequence, 3));
+	}
+	
+	@Test
 	public void giveScoreForNoSparesOrStrikes() throws Exception {
 		ArrayList<Integer> strikeSequence = new ArrayList<Integer>();
 		strikeSequence.addAll(Arrays.asList(9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0,9,0));
@@ -40,6 +54,13 @@ public class BowlingTest {
 		ArrayList<Integer> strikeSequence = new ArrayList<Integer>();
 		strikeSequence.addAll(Arrays.asList(5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5));
 		assertEquals(5, BowlingGame.giveAdditionalPointsForSpare(strikeSequence,4));
+	}
+	
+	@Test
+	public void giveAdditionalPointsForSpareFollowedByZero() throws Exception {
+		ArrayList<Integer> strikeSequence = new ArrayList<Integer>();
+		strikeSequence.addAll(Arrays.asList(5,5,5,5,5,0,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5));
+		assertEquals(0, BowlingGame.giveAdditionalPointsForSpare(strikeSequence,4));
 	}
 	
 	@Test
