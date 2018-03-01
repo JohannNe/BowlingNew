ed4f419c340b975b2f85cdf95f3f34ba6d21a9e7
JohannN <johannneufeld@gmx.net>
2018-03-01 10:06:49 +0100

    Initial implementation for "givingScoreForSequence" so that it calculates the right score for a sequence of strikes only.

--- BowlingTDD/src/BowlingGame.java
+++ BowlingTDD/src/BowlingGame.java
@@ -3,7 +3,32 @@ import java.util.ArrayList;
 public class BowlingGame {
 	
 	public static int giveScoreForSequence(ArrayList<Integer> sequence){
-		return 0;
+		int iScore = 0;
+		int iFrameIndex = 0;
+		int iPins=0;
+
+		boolean bStrike = false;
+		
+		for(int i=0; i<sequence.size(); i++){
+			iPins = sequence.get(i);
+			iScore += iPins;
+			
+			if(iFrameIndex<9){ //No extra points for strikes or spares in the last frame
+				
+				if(iPins==10){ //Player hits strike
+					iScore += giveAdditionalPointsForStrike(sequence,i);
+					bStrike = true;
+				}
+				
+			}
+			
+			if(bStrike){
+				iFrameIndex ++;
+				bStrike = false;
+			}
+		}
+		
+		return iScore;
 	}
 
 	public static int giveAdditionalPointsForStrike(ArrayList<Integer> sequence, int index){
